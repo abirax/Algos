e330aebcd118d134f158606818a2fedd5eee521a
Abir BHattacharyya <abirax@gmail.com>
2019-02-19 10:37:30 -0800

    some comments

--- SumOfPrimes/Program.cs
+++ SumOfPrimes/Program.cs
@@ -8,7 +8,7 @@ namespace SumOfPrimes
      
         static public void Main()
         {
-            //Console.WriteLine("Hello World!") check;
+            //Console.WriteLine("Hello World!cd") check;
             int numberOfTestCases = Int32.Parse(Console.ReadLine());
             int[] mySums = new int[numberOfTestCases];
             for (int k = 0; k < numberOfTestCases; k++)
